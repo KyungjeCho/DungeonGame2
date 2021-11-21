d2a8d716287a013b6203bd056afcddcc516b35f7
Cho, Kyungje <36848308+KyungjeCho@users.noreply.github.com>
2021-11-22 04:01:59 +0900

    Initial commit

---
+++ DungeonGame2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DungeonGame
+{
+    class Program
+    {
+        class Scene
+        {
+            private Screen Screen = new Screen(30, 30);
+            Panel gamePanel;
+            Panel uiPanel;
+
+            Map map;
+            Map map2;
+            Snake snake;
+            TextObject pause;
+
+            Vector2 direction = new Vector2(0, 0);
+            ConsoleKeyInfo keys;
+            bool isStop = false;
+
+            public Scene()
+            {
+                //생성 
+                gamePanel = new Panel(Screen, 0, 0, 20, 20);
+                uiPanel = new Panel(Screen, 20, 0, 10, 10);
+
+                map = new Map(gamePanel);
+                map2 = new Map(uiPanel);
+                snake = new Snake(gamePanel, new Vector2(9, 9));
+                pause = new TextObject(gamePanel, new Vector2(8, 9), "PAUSE");
+                pause.isVisible = false;
+
+                gamePanel.AddGameObject(map);
+                gamePanel.AddGameObject(snake);
+                gamePanel.AddGameObject(pause);
+
+                uiPanel.AddGameObject(map2);
+
+                // 사용할 패널들을 스크린에 추가합니다
+                Screen.AddPanel(gamePanel);
+                Screen.AddPanel(uiPanel);
+
+                while(true)
+                {
+                    Update();
+                    Render();
+                }
+            }
+            
+            private void Update()
+            {
+                Input();
+
+                if(isStop is false)
+                {
+                    snake.isVisible = true;
+                    pause.isVisible = false;
+                    snake.direction = direction;
+                    snake.Move();
+                }
+                else
+                {
+                    snake.isVisible = false;
+                    pause.isVisible = true;
+                }
+
+                Thread.Sleep(snake.speed);
+            }
+
+            private void Render()
+            {
+                Console.SetCursorPosition(0, 0);
+                Screen.Draw();
+                Screen.PrintBufferToScreen();
+            }
+
+            private void Input()
+            {
+                if (Console.KeyAvailable)
+                {
+                    keys = Console.ReadKey(true);
+                    switch (keys.Key)
+                    {
+                        case ConsoleKey.RightArrow:
+                            direction.X = 1;
+                            direction.Y = 0;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            direction.X = -1;
+                            direction.Y = 0;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            direction.X = 0;
+                            direction.Y = -1;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            direction.X = 0;
+                            direction.Y = 1;
+                            break;
+                        case ConsoleKey.S:
+                            if (isStop)
+                                isStop = false;
+                            else
+                                isStop = true;
+                            break;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(200, 50);
+            Console.CursorVisible = false;
+            Scene Scene1 = new Scene();
+        }
+    }
+}
---
+++ DungeonGame2/Utility.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DungeonGame
+{
+    public partial class Vector2
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+        public Vector2(int _x, int _y)
+        {
+            this.X = _x;
+            this.Y = _y;
+        }
+    }
+
+    public partial class Panel
+    {
+        public Vector2 Position { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public List<List<string>> buffer = new List<List<string>>();
+        public List<List<ConsoleColor>> colorMap = new List<List<ConsoleColor>>();
+        public Screen screen;
+        public List<GameObject> gameObjects = new List<GameObject>();
+
+        public Panel(Screen _s, int _x, int _y, int _width, int _height)
+        {
+            this.screen = _s;
+            this.Position = new Vector2(_x, _y);
+            this.Height = _height;
+            this.Width = _width;
+            
+            for (int i = 0; i < this.Height; i++)
+            {
+                List<string> temp = new List<string>();
+                List<ConsoleColor> colorTemp = new List<ConsoleColor>();
+
+                for (int j = 0; j < this.Width; j++)
+                {
+                    temp.Add("  ");
+                    colorTemp.Add(ConsoleColor.White);
+                }
+                buffer.Add(temp);
+                colorMap.Add(colorTemp);
+            }
+        }
+
+        public void AddGameObject(GameObject _go)
+        {
+            gameObjects.Add(_go);
+        }
+
+        private void ResetPanel()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width; j++)
+                {
+                    this.buffer[i][j] = "  ";
+                    this.colorMap[i][j] = ConsoleColor.White;
+                }
+            }
+        }
+        public void Draw()
+        {
+            // Panel reset
+            ResetPanel();
+
+            // 모든 오브젝트 Draw
+            foreach (var go in gameObjects)
+            {
+                go.Draw();
+            }
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    this.screen.buffer
+                        [i + this.Position.Y]
+                        [j + this.Position.X]
+                        = this.buffer[i][j];
+                    this.screen.colorMap
+                        [i + this.Position.Y]
+                        [j + this.Position.X]
+                        = this.colorMap[i][j];
+                }
+            }
+        }
+    }
+
+    public partial class Screen
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+
+        public List<List<string>> buffer = new List<List<string>>();
+        public List<Panel> Panels = new List<Panel>();
+        public List<List<ConsoleColor>> colorMap = new List<List<ConsoleColor>>();
+
+        public Screen(int _height, int _width)
+        {
+            this.Height = _height;
+            this.Width = _width;
+
+            InitializeBuffer();
+        }
+
+        public void AddPanel(Panel _p)
+        {
+            Panels.Add(_p);
+        }
+
+        // 리스트 초기화
+        public void InitializeBuffer()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                List<string> temp = new List<string>();
+                List<ConsoleColor> colorTemp = new List<ConsoleColor>();
+
+                for (int j = 0; j < this.Width; j++)
+                {
+                    temp.Add("  ");
+                    colorTemp.Add(ConsoleColor.White);
+                }
+
+                temp.Add("\n");
+                buffer.Add(temp);
+
+                colorTemp.Add(ConsoleColor.White);
+                colorMap.Add(colorTemp);
+            }
+        }
+
+        // 스크린 버퍼 리셋
+        public void ResetBuffer()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width; j++)
+                {
+                    buffer[i][j] = "  ";
+                }
+            }
+        }
+
+        // 스크린 버퍼 -> 콘솔 출력 
+        public void PrintBufferToScreen()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width + 1; j++)
+                {
+                    Console.ForegroundColor = colorMap[i][j];
+                    Console.Write(buffer[i][j]);
+                }
+
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (var p in Panels)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
